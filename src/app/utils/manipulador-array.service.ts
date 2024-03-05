7879ae7a7db9b17cfb7a27418b65bb9f92c4e439
Cristian Russy <91230251+Cristianss11@users.noreply.github.com>
2024-03-05 18:14:01 -0300

    Update manipulador-array.service.ts
    Fiz uma mudança aqui para diminuir os códigos no gráfico-entradas-mes e talvez nós outros gráficos pois criei método de compara datas, e calcular totais o que pode diminuir um pouco

--- src/app/utils/manipulador-array.service.ts
+++ src/app/utils/manipulador-array.service.ts
@@ -42,9 +42,35 @@ export class ManipuladorArrayService {
     return array;
   }
 
+    /**!this.faturamentoDiario.some(faturamento => faturamento.data === entrada.data);
+   * Dentro da função de filtro, estamos usando some no array this.faturamentoDiario.
+   * O método some verifica se pelo menos um elemento no array satisfaz a condição especificada.
+   * Estamos verificando se não há nenhum faturamento cuja data corresponda à data da entrada atual.
+   * Se nenhum faturamento tiver a mesma data da entrada, isso significa que é uma data diferente e,portanto, retornamos true, caso contrário,retornamos false.
+  **/
+  public verificarDifenreçaDeAtributoDataEntreLista(lista: any, lista2: any) {
+    let diferença = lista.filter((entrada: any) => !lista2.some((valor: any) => valor.data === entrada.data));
+    diferença.forEach((v: any) => {
+      lista2.push({
+        id: v.id,
+        data: v.data,
+        valor: 0,
+      });
+    });
+    return lista2;
+  }
 
-  public verificadorArray(){
-    
+  public calcularValoresGraficos(lista: any, listaData?: any[]) {
+    let listaValores: any[] = [];
+    lista.forEach((e: any) => {
+      listaValores.push(e.valor);
+      listaData?.push(moment(e.data).date());
+    });
+    return listaValores;
+  }
+
+  public ordernarArrayPorData(lista: any) {
+    return lista.sort((a: any, b: any) => b.data.localeCompare(a.data));
   }
 
 }
